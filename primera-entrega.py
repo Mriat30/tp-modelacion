67251d022e2d10dbac7ea6d48322bb23fb619d0e
mreimundo <mreimundo@fi.uba.ar>
2024-11-05 23:29:08 -0300

    hallamos c2 y aclaro partes

--- primera-entrega.py
+++ primera-entrega.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-# Definimos las constantes
+# Definimos las constantes según nuestro caso del modelo
 x0 = -7.5  # Asumiendo que x0 = -x1
 x1 = 7.5   # Asumiendo que x1 = -x0
 L = 15.0   # Longitud entre los puntos (ajusta este valor según el problema)
@@ -16,7 +16,7 @@ def df(mu):
 
 # Valor inicial para mu y tolerancia
 mu = 0.1  # Semilla inicial (puedes ajustar este valor)
-tolerance = 0.5e-3
+tolerance = 0.5e-4 #Comparado al error inherente tomado (espesor) se considera casi despreciable
 
 # Newton-Raphson loop
 iteration = 0
@@ -27,9 +27,12 @@ while True:
     mu = mu_new
     iteration += 1
 
+#Cálculo del C2
+y1 = 23.0 #altura del soporte de la derecha (igual a y0)
+C2 = y1*mu - (np.cosh(mu*x1)) #despejando de la ecuación (3)
+
+
 # Resultados
 print("Valor de mu:", mu)
 print("Número de iteraciones:", iteration)
-
-
-
+print("Valor de C2", C2)
